2fdd75e2e7b14faa8e2c0a764cde86d42848dfc5
Boryana <boryana.hadzhiyska@cfa.harvard.edu>
2023-03-17 15:49:21 -0700

    changed the downsampling scheme

--- sigma/compute_sigma.py
+++ sigma/compute_sigma.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import numpy as np
 import asdf
+import random
 from scipy.interpolate import interp1d
 
 from halotools.mock_observables import mean_delta_sigma
@@ -26,51 +27,78 @@ def get_sigma_crit(z_l, z_s, d_l, d_s, comoving=True):
         dist_term /= (1.0 + z_l)**2 # cMpc^-1 / (1+z_l)
     return _sigma_crit_factor * dist_term # Msun/cMpc^2
 
-def load_halo_field_down(sim_dir, z, n_chunks, N_parts, f_down):
-    N_goal = int(1.05*N_parts/f_down)
-    pos_down = np.zeros((N_goal, 3), dtype=np.float32)
-    vel_down = np.zeros((N_goal, 3), dtype=np.float32)
+def load_halo_field_down(sim_dir, z, n_chunks, N_parts, f_down, type_AB='A'):
+    # estimate total number of particles and preselect indices
+    N_all = 0
+    N_offset = np.zeros(n_chunks, dtype=int)
+    N_file = np.zeros(n_chunks, dtype=int)
+    for i_chunk in range(n_chunks):
+        print(i_chunk, n_chunks)
+
+        # halo and field particles
+        fn_halo = sim_dir+f'/halos/z{z:.3f}/halo_rv_{type_AB}/halo_rv_{type_AB}_{i_chunk:03d}.asdf'
+        fn_field = sim_dir+f'/halos/z{z:.3f}/field_rv_{type_AB}/field_rv_{type_AB}_{i_chunk:03d}.asdf'
+        N_this = asdf.open(fn_halo)['data']['rvint'].shape[0]+asdf.open(fn_field)['data']['rvint'].shape[0]
+        N_offset[i_chunk] = N_all
+        N_file[i_chunk] = N_this
+        N_all += N_this
+    gc.collect()
+    print("offsets", N_offset)
+    print("per file", N_file)
+    print("all the particles in the halo and field files", N_all)
+
+    # global indices to keep
+    #inds_keep = np.arange(N_all)
+    #np.random.shuffle(inds_keep)
+    #inds_keep = inds_keep[::f_down]
+    inds_keep = random.sample(range(N_all), N_all//f_down)
+    N_keep = len(inds_keep)
+    print("N_keep", N_keep)
+    pos_down = np.zeros((N_keep, 3), dtype=np.float32)
+    vel_down = np.zeros((N_keep, 3), dtype=np.float32)
     
     # load the matter particles
     count = 0
     for i_chunk in range(n_chunks):
         print(i_chunk, n_chunks)
-        for type_AB in ['B']:
-            # halo and field particles
-            fn_halo = sim_dir+f'/halos/z{z:.3f}/halo_rv_{type_AB}/halo_rv_{type_AB}_{i_chunk:03d}.asdf'
-            fn_field = sim_dir+f'/halos/z{z:.3f}/field_rv_{type_AB}/field_rv_{type_AB}_{i_chunk:03d}.asdf'
+        # indices to keep in this chunk
+        inds_keep_this = inds_keep - N_offset[i_chunk]
+        inds_keep_this = inds_keep_this[(inds_keep_this >= 0) & (inds_keep_this < N_file[i_chunk])]
+        
+        # halo and field particles
+        fn_halo = sim_dir+f'/halos/z{z:.3f}/halo_rv_{type_AB}/halo_rv_{type_AB}_{i_chunk:03d}.asdf'
+        fn_field = sim_dir+f'/halos/z{z:.3f}/field_rv_{type_AB}/field_rv_{type_AB}_{i_chunk:03d}.asdf'
 
-            # write out the halo (L0+L1) matter particles
-            halo_data = (asdf.open(fn_halo)['data'])['rvint']
-            pos_halo, vel_halo = unpack_rvint(halo_data, Lbox, float_dtype=np.float32, velout=None)
-            #pos_halo += Lbox/2. # TESTING
-            print("pos_halo = ", pos_halo[:5])
-            inds = np.arange(pos_halo.shape[0])
-            np.random.shuffle(inds)
-            inds = inds[::f_down]
-            pos_down[count:count+len(inds)] = pos_halo[inds]
-            vel_down[count:count+len(inds)] = vel_halo[inds]
-            count += len(inds)
-            del halo_data, pos_halo, vel_halo
-            gc.collect()
+        # write out the halo (L0+L1) matter particles
+        halo_data = (asdf.open(fn_halo)['data'])['rvint']
+        pos_halo, vel_halo = unpack_rvint(halo_data, Lbox, float_dtype=np.float32, velout=None)
+        print("pos_halo = ", pos_halo[:5])
 
-            # write out the field matter particles
-            field_data = (asdf.open(fn_field)['data'])['rvint']
-            pos_field, vel_field = unpack_rvint(field_data, Lbox, float_dtype=np.float32, velout=None)
-            #pos_field += Lbox/2. # TESTING
-            print("pos_field = ", pos_field[:5])
-            pos_field = pos_field[::f_down]
-            inds = np.arange(pos_field.shape[0])
-            np.random.shuffle(inds)
-            inds = inds[::f_down]
-            pos_down[count:count+len(inds)] = pos_field[inds]
-            vel_down[count:count+len(inds)] = vel_field[inds]
-            count += len(inds)
-            del field_data, pos_field, vel_field
-            gc.collect()
+        # write out the field matter particles
+        field_data = (asdf.open(fn_field)['data'])['rvint']
+        pos_field, vel_field = unpack_rvint(field_data, Lbox, float_dtype=np.float32, velout=None)
+        print("pos_field = ", pos_field[:5])
+
+        # stack halo and field particles
+        pos_both = np.vstack((pos_halo, pos_field))
+        vel_both = np.vstack((vel_halo, vel_field))
+
+        # additional randomization cause am freaking out
+        inds = np.arange(pos_both.shape[0])
+        np.random.shuffle(inds)
+        pos_both = pos_both[inds]
+        vel_both = vel_both[inds]
+        #inds = inds[::f_down]
+
+        # write out pos and vel
+        pos_down[count:count+len(inds_keep_this)] = pos_both[inds_keep_this]
+        vel_down[count:count+len(inds_keep_this)] = vel_both[inds_keep_this]
+        count += len(inds_keep_this)
+        del halo_data, pos_halo, vel_halo, field_data, pos_field, vel_field
+        gc.collect()
+    print("these two must be the same", count, pos_down.shape[0])
     pos_down = pos_down[:count]
     vel_down = vel_down[:count]
-    print("goal vs reality", N_goal, pos_down.shape[0], N_goal/count)
     return pos_down, vel_down
 
 # specify parameters
@@ -89,10 +117,10 @@ scratch_dir = "/global/cscratch1/sd/boryanah/abacus_lensing/"
 # galaxy and pcle parameters
 want_rsd = True
 rsd_str = "_rsd" if want_rsd else ""
-tracer = "ELG"; z_l = 0.8; z_s = 1.4
-#tracer = "LRG"; z_l = 0.5; z_s = 1.025 # missing
+#tracer = "ELG"; z_l = 0.8; z_s = 1.4
+tracer = "LRG"; z_l = 0.5; z_s = 1.025 
 fn_gals = f"{scratch_dir}/mocks_box_output/{sim_name}/z{z_l:.3f}/galaxies{rsd_str}/{tracer}s.dat"
-sim_dir = f"{scratch_dir}/{sim_name}/"
+sim_dir = f"/global/project/projectdirs/desi/cosmosim/Abacus/{sim_name}/" #f"{scratch_dir}/{sim_name}/"
 n_chunks = 34
 
 # location of headers
@@ -132,8 +160,8 @@ print("number of galaxies", N_gal)
 print("min max galaxies", pos_gals.min(), pos_gals.max())
 
 # load particles
-f_down = 50 # 30 as many as galaxies
-parts_fn = f"{sim_dir}/pos_vel_parts_z{z_l:.3f}_{f_down:d}.npy.npz"
+f_down = 1000 # 30 as many as galaxies
+parts_fn = f"{sim_dir}/pos_vel_parts_z{z_l:.3f}_{f_down:d}.npz"
 if os.path.exists(parts_fn):
     data = np.load(parts_fn)
     pos_parts = data['pos_parts']
@@ -143,13 +171,6 @@ else:
     np.savez(parts_fn, pos_parts=pos_parts, vel_parts=vel_parts)
 N_part = pos_parts.shape[0]
 print("downsampled particles more than galaxies by", N_part/N_gal)
-
-# true downsampling
-f_down = N_parts/N_part
-print("true downsampling factor", f_down)
-
-# adjust effective particle mass
-pcle_mass *= f_down
 
 # specify R bins
 rp_bins = np.logspace(-1.5, 2, 31) # cMpc/h
@@ -170,16 +191,26 @@ if want_rsd:
 pos_gals %= Lbox
 pos_parts %= Lbox
 
-# TESTING!!!!!!!!!!!!
-inds = np.arange(pos_parts.shape[0])
-np.random.shuffle(inds)
-inds = inds[::10]
+# further downsampling by a factor of X
+print("additional downsampling")
+X = 2
+#inds = np.arange(pos_parts.shape[0])
+#np.random.shuffle(inds)
+#inds = inds[::X]
+inds = random.sample(range(pos_parts.shape[0]), pos_parts.shape[0]//X)
 pos_parts = pos_parts[inds]
-pcle_mass *= 10.
+
+# true downsampling
+N_part = pos_parts.shape[0]
+f_down = N_parts/N_part
+print("true downsampling factor", f_down)
+
+# adjust effective particle mass
+pcle_mass *= f_down
 
 # compute from the box
 Delta_Sigma = mean_delta_sigma(pos_gals, pos_parts, pcle_mass, rp_bins, period, num_threads=n_thread) # hMsun/cMpc^2
-# pretty much the same amplitude but offset?
+# pretty much the same amplitude but offset -- need to do derivative
 """
 pimax = 500 # 1000 complains
 wp = calc_wp_fast(pos_gals[:, 0], pos_gals[:, 1], pos_gals[:, 2], rp_bins, pimax,
